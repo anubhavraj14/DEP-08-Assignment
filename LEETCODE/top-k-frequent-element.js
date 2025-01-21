dedb8365d1db63972066ccbab351ed485b284b6d
anubhavraj14 <98094724+anubhavraj14@users.noreply.github.com>
2025-01-21 16:33:03 +0530

    Update top-k-frequent-element.js

--- LEETCODE/top-k-frequent-element.js
+++ LEETCODE/top-k-frequent-element.js
@@ -1,1 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const myMap = new Map();
 
+    nums.forEach(num=>{
+        myMap.set(num, (myMap.get(num) || 0) + 1);
+    });
+
+    const sortFreq = Array.from(myMap.entries()).sort((a,b) => b[1]-a[1]);
+
+    return sortFreq.slice(0, k).map(pair => pair[0]);
+
+
+
+};
